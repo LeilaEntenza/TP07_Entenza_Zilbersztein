a11d4ee793d620f9a96ff68c3a23a6be4a301927
Android Studio <devandroid@ort>
2025-06-24 09:30:28 -0300

    quienes somos

---
+++ src/pages/QuienesSomos.jsx
@@ -0,0 +1,28 @@
+    import React from 'react'
+    
+    function QuienesSomos() {
+      return (
+        <>
+            <div className='QuienesSomosInicio'>
+                <p>Información de la compañía</p>
+                <h1>Buscamos brindar productos de calidad a nuestros usuarios cada día</h1>
+            </div>
+            <p>Trabajamos cada día para mejorar nuestra compañía</p>
+            <div className='mision'>
+                <h1>Nuestra misión</h1>
+                <p>Desde 1970 nos esforzamos para llevar productos de calidad y que traigan felicidad a los argentinos</p>
+            </div>
+            <h1>Conocé al equipo</h1>
+            <div className='persona'>
+                <img className='imgPersona' src=""/>
+                <h1>Nombre</h1>
+            </div>
+            <div className='persona'>
+                <img className='imgPersona' src=""/>
+                <h1>Nombre</h1>
+            </div>
+        </>
+      )
+    }
+    
+    export default QuienesSomos
