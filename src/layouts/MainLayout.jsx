2d9ad9972ebe5073b091df3022b695ee9a6a25cf
MartinZilber <48520155@est.ort.edu.ar>
2025-07-15 10:14:15 -0300

    Subiendo

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -10,14 +10,14 @@ export const MainLayout = () => {
       <header>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/TP07_Entenza_Zilbersztein/Home">ORT Market</a>
+            <a className="navbar-brand" href="/TP07_Entenza_Zilbersztein/home">ORT Market</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <Link className="nav-link active" to="/home">Home</Link>
+                  <Link className="nav-link active" to="/TP07_Entenza_Zilbersztein/home">Home</Link>
                 </li>
                 <li className="nav-item dropdown">
                   <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
