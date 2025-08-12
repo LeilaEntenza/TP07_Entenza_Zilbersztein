b769ff0d0f7885c6d7cb7d14502b02c35f74d3b7
Android Studio <devandroid@ort>
2025-08-12 09:53:38 -0300

    funca el cosito del carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,21 +1,27 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { CartContext } from '../context/CartContext'
 import './CartWidget.css'
 
 export const CartWidget = () => {
-  const {cartItems} = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+
   return (
-    <div class="dropdown">
-    <button class="btn btn-secondary carrito" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart" viewBox="0 0 20 20">
-              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-          </svg>
-          <p className="cant">{cartItems.length}</p>    </button>
-    <ul class="dropdown-menu">
-      <li><div className="Producto">
-        <p>Producto</p>
-        </div></li>
-    </ul>
-  </div>
-  )
-}
+    <div className="dropdown">
+      <button className="btn btn-secondary carrito" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-cart" viewBox="0 0 20 20">
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+        </svg>
+        <span className="cant">{cartItems.length}</span>
+      </button>
+      <ul className="dropdown-menu dropdown-menu-end">
+        <p className='pProd'><b>Carrito</b></p>        
+        <li>
+          <div className="Producto">
+            <p className='pProd'>Producto</p>
+            <img width="100%" src="https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/12858/production/_130346857_capture.png"/>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
