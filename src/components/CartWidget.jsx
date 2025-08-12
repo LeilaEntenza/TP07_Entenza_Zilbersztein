b2ffb30ae1e68b768b52a28d1f86f5ba3f5d89fc
Android Studio <devandroid@ort>
2025-08-12 10:03:55 -0300

    Burger menu

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,27 +1,45 @@
-import React, { useContext } from 'react'
-import { CartContext } from '../context/CartContext'
-import './CartWidget.css'
+import React, { useContext, useState } from 'react';
+import { CartContext } from '../context/CartContext';
+import './CartWidget.css';
 
 export const CartWidget = () => {
   const { cartItems } = useContext(CartContext);
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
 
   return (
-    <div className="dropdown">
-      <button className="btn btn-secondary carrito" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-cart" viewBox="0 0 20 20">
-          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+    <>
+      <div className="carrito" onClick={toggleMenu}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart" viewBox="0 0 20 20">
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
         </svg>
         <span className="cant">{cartItems.length}</span>
-      </button>
-      <ul className="dropdown-menu dropdown-menu-end">
-        <p className='pProd'><b>Carrito</b></p>        
-        <li>
-          <div className="Producto">
-            <p className='pProd'>Producto</p>
-            <img width="100%" src="https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/12858/production/_130346857_capture.png"/>
-          </div>
-        </li>
-      </ul>
-    </div>
+      </div>
+
+      {/* Sidebar menu */}
+      <div className={`sidebar ${open ? 'open' : ''}`}>
+        <div className="sidebar-header">
+          <h4>Carrito</h4>
+          <button onClick={toggleMenu} className="close-btn">&times;</button>
+        </div>
+        <div className="sidebar-content">
+          {cartItems.length === 0 ? (
+            <p>No hay productos en el carrito.</p>
+          ) : (
+            cartItems.map((item, index) => (
+              <div key={index} className="producto">
+                <p>{item.nombre} x {item.cantidad}</p>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+
+      {/* Fondo oscuro */}
+      {open && <div className="overlay" onClick={toggleMenu}></div>}
+    </>
   );
 };
