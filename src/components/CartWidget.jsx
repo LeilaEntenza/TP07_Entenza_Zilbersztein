07b3088f14a67ce6a964a4bc9e5a453627c7fd15
Android Studio <devandroid@ort>
2025-08-19 08:18:06 -0300

    Carrito con scroll

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -43,6 +43,22 @@ export const CartWidget = () => {
             <p className='carritoP'>Producto 2</p>
             <img width="100%" src="https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/12858/production/_130346857_capture.png"/>
           </div>
+          <div className='producto'>
+              <p className='carritoP'>Producto 1</p>
+              <img width="100%" src="https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/12858/production/_130346857_capture.png"/>
+              <QuantityInput/>
+          </div>
+          <div className='producto'>
+              <p className='carritoP'>Producto 1</p>
+              <img width="100%" src="https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/12858/production/_130346857_capture.png"/>
+              <QuantityInput/>
+          </div>
+          <div className='producto'>
+              <p className='carritoP'>Producto 1</p>
+              <img width="100%" src="https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/12858/production/_130346857_capture.png"/>
+              <QuantityInput/>
+          </div>
+          <a href='resumen'><button>Abrir resumen</button></a>
         </div>
       </div>
       {open && <div className="overlay" onClick={toggleMenu}></div>}
